83aaa802b474a570ef7b02a579c582a851fd4656
josandg <gussanjoba@student.gu.se>
2023-04-16 21:53:13 +0200

    Starter files &  Project structure

---
+++ IoT-Wio/main/main.ino
@@ -0,0 +1,19 @@
+// Wifi details, import from header file.
+const char* ssid = SSID
+const char* password = PASSWORD
+
+// MQTT Broker URL
+const char* mqtt_server = my_IPv4
+
+
+
+
+void setup () {
+  
+}
+
+
+void loop() {
+  
+  
+}
