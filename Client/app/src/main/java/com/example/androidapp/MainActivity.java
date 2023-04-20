35616aa419f6cfec59e95e73a37b7a712d4fa591
josandg <gussanjoba@student.gu.se>
2023-04-20 18:42:40 +0200

    Settings page UI & navigation from dashboard

--- Client/app/src/main/java/com/example/androidapp/MainActivity.java
+++ Client/app/src/main/java/com/example/androidapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidapp;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -23,11 +24,18 @@ public class MainActivity extends AppCompatActivity {
 
         // HOME BUTTON SETTINGS
         homeButton = findViewById(R.id.home_button);
+        // create a onClickListener for the home button and when its clicked  a new intent is created
+        // which navigates to SettingsActivity
+
         homeButton.setOnClickListener(view -> Toast.makeText(getApplicationContext(), "Home pressed", Toast.LENGTH_SHORT).show());
 
         // SETTINGS BUTTON SETTINGS
         settingsButton = findViewById(R.id.settings_button);
-        settingsButton.setOnClickListener(view -> Toast.makeText(getApplicationContext(), "Settings pressed", Toast.LENGTH_SHORT).show());
+        settingsButton.setOnClickListener(view -> {
+                    Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                    startActivity(intent);  // start settings activity
+                }
+        );
 
         // HISTORY BUTTON SETTINGS
         historyButton = findViewById(R.id.history_button);
