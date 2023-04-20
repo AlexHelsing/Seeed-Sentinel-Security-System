19225b74a360e5d5a21e6a893911542e96f45709
Alexander <helsinga@student.chalmers.se>
2023-04-21 01:42:27 +0200

    fix dashboard buttons so that you can push whole button and not only the icon

--- Client/app/src/main/java/com/example/androidapp/MainActivity.java
+++ Client/app/src/main/java/com/example/androidapp/MainActivity.java
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView homeButton;
-    ImageView settingsButton;
-    ImageView historyButton;
+    LinearLayout homeButton;
+    LinearLayout settingsButton;
+    LinearLayout historyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
