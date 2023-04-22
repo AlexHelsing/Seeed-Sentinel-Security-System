d688a94eec472a49336b6dc3d0192d900bd61244
josandg <gussanjoba@student.gu.se>
2023-04-22 14:33:07 +0200

    removed unused imports

--- Client/app/src/main/java/com/example/androidapp/MainActivity.java
+++ Client/app/src/main/java/com/example/androidapp/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.androidapp;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
