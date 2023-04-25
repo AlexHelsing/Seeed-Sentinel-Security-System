5a9ae8d75a429193df82127f0bae686f84bb72e6
josandg <gussanjoba@student.gu.se>
2023-04-25 20:41:06 +0200

    implemented customized data to append to the user profiles
    we need to create customized collections for each user to store stuff like name, profile-pic, the pattern etc.. https://www.mongodb.com/docs/realm/sdk/java/users/custom-user-data/

--- Client/app/src/main/java/com/example/androidapp/CreateAccount.java
+++ Client/app/src/main/java/com/example/androidapp/CreateAccount.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import io.realm.mongodb.App;
+import io.realm.mongodb.Credentials;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -19,6 +20,11 @@ public class CreateAccount extends AppCompatActivity {
         dbHandler db = new dbHandler(getApplicationContext());
         App app = db.getApp();
 
+        if (app.currentUser() != null) {
+            Intent intent = new Intent(getApplicationContext(), CustomizeProfile.class);
+            startActivity(intent);
+        }
+
         EditText email = findViewById(R.id.ca_email);
         EditText password = findViewById(R.id.ca_password);
 
@@ -28,6 +34,28 @@ public class CreateAccount extends AppCompatActivity {
             String emailText = email.getText().toString();
             String passwordText = password.getText().toString();
             Log.v("CreateAccount", "email: " + emailText + " password: " + passwordText);
+
+            app.getEmailPassword().registerUserAsync(emailText, passwordText, it -> {
+                if (it.isSuccess()) {
+                    Log.v("CreateAccount", "Successfully registered user.");
+
+                    Credentials credentials = Credentials.emailPassword(emailText, passwordText);
+                    app.loginAsync(credentials, res -> {
+                        if (res.isSuccess()) {
+                            Log.v("CreateAccount", "Successfully logged in.");
+                            // not sure where we should send users after creating account yet.
+                            Intent intent = new Intent(getApplicationContext(), CustomizeProfile.class);
+                            startActivity(intent);
+                        } else {
+                            Log.v("CreateAccount", "Failed to log in: " + res.getError().getErrorMessage());
+                            Toast.makeText(getApplicationContext(), "Failed to log in: " + res.getError().getErrorMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    Log.v("CreateAccount", "Failed to register user: " + it.getError().getErrorMessage());
+                    Toast.makeText(getApplicationContext(), "Failed to register user: " + it.getError().getErrorMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
         });
     }
 }
