f51707490274073a6f2a7e3226b9f0f4d5e4bacf
josandg <gussanjoba@student.gu.se>
2023-05-07 15:55:37 +0200

    fixed minor type error

--- Client/app/src/main/java/com/example/androidapp/createAccount/CreateAccountActivity.java
+++ Client/app/src/main/java/com/example/androidapp/createAccount/CreateAccountActivity.java
@@ -1,10 +1,12 @@
 package com.example.androidapp.createAccount;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.androidapp.R;
@@ -18,10 +20,11 @@ public class CreateAccountActivity extends AppCompatActivity {
     dbHandler db;
     App app;
     Button createAccountButton;
-    Button goBackButton;
+    ImageView goBackButton;
     EditText email;
     EditText password;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
